8c77867bd03ea5aefb18a8218922735b68bc450d
as7740 <as7740@MSS2150-20>
2021-09-22 13:02:45 -0400

    dot prod multithreaded calc.

--- DotProductExample.cpp
+++ DotProductExample.cpp
@@ -24,9 +24,37 @@ void dotProductCalc(const std::vector<int>& v1, const std::vector<int>& v2, int&
         partial_sum += v1[i] * v2[i];
     }
 
+    std::unique_lock<std::mutex> lock(dotMtx);
+    dotCV.wait(lock, []() {return ready; });
    
     result += partial_sum;
+    lock.unlock();
+    dotCV.notify_one();
 
+}
+
+std::vector<int> calcLimits(int numTerms, int numThreads) {
+    std::vector<int> limits;
+    int delta = numTerms / numThreads;
+    int remaining = numTerms % numThreads;
+
+    int start = 0;
+    int step = 0;
+
+    limits.push_back(start);
+
+    for (int i = 0; i < numThreads; i++) {
+        step += delta;
+
+        if (i == numThreads - 1) {
+            step += remaining;
+        }
+
+        limits.push_back(step);
+        start = step;
+    }
+
+    return limits;
 }
 
 int main()
@@ -41,8 +69,12 @@ int main()
     std::vector<int> v1(numElements, 1);
     std::vector<int> v2(numElements, 3);
 
+    std::vector<int> limits = calcLimits(numElements, numThreads);
+
     /*Fire off your threads here and do this calculation over a number of threads*/
-    dotProductCalc(v1, v2, result, 0, numElements);
+    for (int i = 0; i < numThreads; i++) {
+        threads.push_back(std::thread(dotProductCalc, std::ref(v1), std::ref(v2), std::ref(result), limits[i], limits[i + 1]));
+    }
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
@@ -50,10 +82,13 @@ int main()
     ready = true;
     std::cout << "Fire signal for data ready to be processed" << std::endl;
     lock.unlock();
-
+    dotCV.notify_one();
     /*Call notify here*/
 
     /*Remember to join threads with main threads*/
+    for (auto& thread : threads) {
+        thread.join();
+    }
 
     std::cout << "Result:" << result << std::endl;
 }
